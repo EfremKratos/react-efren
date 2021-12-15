2126abec8417a48502e9d847fa91712c9502f4a5
EfremKratos <efrenximenes889@outlook.com>
2021-12-15 12:00:49 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-oweglr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-oweglr)
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  const style = {
+    color: 'red',
+  };
+  return (
+    <div>
+      <h1 color={style}>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
